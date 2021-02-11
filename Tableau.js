5b325150d57bc57e51046798f34792e77d6933b9
Sreejadhar14 <44405018+Sreejadhar14@users.noreply.github.com>
2021-02-11 23:07:13 +0530

    Add files via upload

---
+++ Tableau.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+function initViz(){
+	var ContainerDiv= document.getElementbyId("Tableau Analysis"),
+	url="https://public.tableau.com/views/ExploringDataAnalysisofLendingClubsloan/ExploringDataAnalysisofLendingClubsloan?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
+	var viz= new tableau.Viz(ContainerDiv, url)
+}
+               
